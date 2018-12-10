8c610d935fff7d7c5d28b15154b07d6c45805b58
Anna <annfr542@student.liu.se>
2018-12-10 14:24:32 +0100

    data story

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function (){
+    $('a[href^="#"]').on('click', function(event) {
+        let target = $(this.getAttribute('href'));
+        var menuHeight = $("div#menu").height() + 20;
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top - menuHeight
+            }, 1000);
+        }
+    });
+
+    window.onscroll = function() {scrollFunction()};
+
+    let menu = document.getElementById("menu");
+    let sticky = menu.offsetTop;
+
+    function scrollFunction() {
+        if (window.pageYOffset > sticky) {
+            menu.classList.add("sticky");
+        } else {
+            menu.classList.remove("sticky");
+        }
+    }
+});
