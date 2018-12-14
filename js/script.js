e16b071b5bbfeff93a0755ffc0b31c3385181f9d
Anna <annfr542@student.liu.se>
2018-12-14 10:50:48 +0100

    Dropdown list for plots

--- js/script.js
+++ js/script.js
@@ -23,3 +23,17 @@ $(document).ready(function (){
         }
     }
 });
+
+// Intialize plots
+showPlot('fats_saturated-fat','fatsPlots');
+showPlot('snacks_sugars','snacksPLots');
+
+
+function showPlot(imgId,plotClass){
+    let x = document.getElementsByClassName(plotClass);
+    for (let i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    let selectedImg = document.getElementById(imgId);
+    selectedImg.style.display = "block"; 
+}
